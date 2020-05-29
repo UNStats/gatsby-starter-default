001d572b07766c04b75453f614011081db71e517
Paul Narain <narain@un.org>
2020-05-29 14:59:00 -0400

    Create heading Fixes #4

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -10,6 +10,7 @@ const SecondPage = () => (
     <h1>Hi from the second page</h1>
     <p>Welcome to page 2</p>
     <Link to="/">Go back to the homepage</Link>
+    <h1>Paul's heading</h1>{" "}
   </Layout>
 )
 
