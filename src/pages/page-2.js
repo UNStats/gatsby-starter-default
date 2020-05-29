51f0257905b84a8dac29c7e2c24b35dfefe2be39
Thilo Maier <git@maier.asia>
2020-05-29 14:50:51 -0400

    Create heading Fixes #3

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -10,7 +10,9 @@ const SecondPage = () => (
     <h1>Hi from the second page</h1>
     <p>Welcome to page 2</p>
     <Link to="/">Go back to the homepage</Link>
-    <h1>Paul's heading</h1>{" "}
+    <h1>Paul's heading</h1>
+
+    <h1>Thilo's heading</h1>
   </Layout>
 )
 
